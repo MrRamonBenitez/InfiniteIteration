1ce08c2e3055b96644a8b1dc2f4dbf66cdeb0186
MrRamonBenitez <mr.ramon.benitez@gmail.com git config --global user.email mr.ramon.benitez@gmail.com git config --global user.email mr.ramon.benitez@gmail.com>
2022-02-19 22:23:50 +0500

    Class Randoms added

--- src/Randoms.java
+++ src/Randoms.java
@@ -1,16 +1,30 @@
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 public class Randoms implements Iterable<Integer>{
     protected Random random;
 
     public Randoms(int min, int max) {
-        //...
+        new ArrayList<>(new Random().ints(min, max)
+                .boxed()
+                .collect(Collectors.toList()));
     }
 
-    //...
     @Override
     public Iterator<Integer> iterator() {
-        return null;
+        return new Iterator<Integer>() {
+            @Override
+            public boolean hasNext() {
+                return false;
+            }
+
+            @Override
+            public Integer next() {
+                return null;
+            }
+        };
+
     }
 }
