881eae5ef0d957d774f56ee206d2e45ecdaec376
MrRamonBenitez <mr.ramon.benitez@gmail.com git config --global user.email mr.ramon.benitez@gmail.com git config --global user.email mr.ramon.benitez@gmail.com>
2022-02-26 00:08:44 +0500

    Class Randoms update

--- src/Randoms.java
+++ src/Randoms.java
@@ -1,39 +1,19 @@
 import java.util.Iterator;
-import java.util.List;
 import java.util.Random;
-import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Randoms implements Iterable<Integer> {
     protected Random random;
-    protected List<Integer> list;
+    private final IntStream stream;
 
     public Randoms(int min, int max) {
         this.random = new Random();
-        this.list = random.ints((max - min), min, max + 1)
-                .boxed()
-                .collect(Collectors.toList());
+        stream = random.ints((max - min), min, max + 1);
     }
 
     @Override
     public Iterator<Integer> iterator() {
-        return new Iterator<>() {
-            int nextItem = 0;
-
-            @Override
-            public boolean hasNext() {
-                return nextItem < list.size();
-            }
-
-            @Override
-            public Integer next() {
-                if (nextItem < list.size()) {
-                    int element = list.get(nextItem);
-                    nextItem++;
-                    return element;
-                } else { return null; }
-            }
-
-        };
+        return stream.iterator();
     }
 }
 
